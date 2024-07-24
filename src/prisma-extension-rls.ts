8acbdbb33f0d2fb6d6d8b2ebba3e6d5752b8c393
z <haig8@msn.com>
2024-07-24 13:58:22 +0800

    feat: add context without types to args for $allOperations

--- src/prisma-extension-rls.ts
+++ src/prisma-extension-rls.ts
@@ -1,5 +1,7 @@
 import { Prisma } from '@prisma/client';
 
+type ContextData = Record<string, unknown> | [string, unknown][];
+
 const stringify = (v: unknown = null) => {
   return v !== null
     ? typeof v === 'object'
@@ -8,21 +10,31 @@ const stringify = (v: unknown = null) => {
     : null;
 };
 
-export function createContext(configs: [string, string][]) {
+const mergeContext = (...args: (ContextData | undefined)[]) =>
+  args.flatMap((arg) =>
+    arg ? (Array.isArray(arg) ? arg : Object.entries(arg)) : []
+  );
+
+export function createContext(initial: ContextData) {
   return Prisma.defineExtension((client) =>
     client.$extends({
       name: 'context',
       query: {
         $allModels: {
           async $allOperations({ args, query }) {
-            const sqlParts = configs.map(
-              ([k, v]) => Prisma.sql`SET_CONFIG(${k}, ${v}, TRUE)`
+            const { context, ...rest } = args as typeof args & {
+              context?: ContextData;
+            };
+
+            const merged = mergeContext(initial, context);
+            const sqlParts = merged.map(
+              ([k, v]) => Prisma.sql`SET_CONFIG(${k}, ${stringify(v)}, TRUE)`
             );
             const sql = Prisma.sql`SELECT ${Prisma.join(sqlParts)}`;
 
             const [, result] = await client.$transaction([
               client.$executeRaw(sql),
-              query(args),
+              query(rest),
             ]);
             return result;
           },
@@ -36,11 +48,8 @@ export const rlsExtension = Prisma.defineExtension((client) =>
   client.$extends({
     name: 'rls',
     client: {
-      $rls(input: Record<string, unknown> | [string, unknown][]) {
-        const data = (Array.isArray(input) ? input : Object.entries(input)).map(
-          ([k, v]) => [k, stringify(v)] satisfies [string, string]
-        );
-        return client.$extends(createContext(data));
+      $rls(input: ContextData) {
+        return client.$extends(createContext(input));
       },
     },
   })
